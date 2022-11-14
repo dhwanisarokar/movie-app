f39e6bfae6e4105d080c12a8da86291466d74463
dhwanisarokar <dhwanisarokar@gmail.com>
2022-11-14 19:45:07 +0530

    Add UI, actons and reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import MovieCart from "./MovieCart";
+import Navbar from "./Navbar";
+import { data } from "../data";
 function App() {
-  return <div className="App">Project start Up</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favorites</div>
+        </div>
+      </div>
+
+      <div className="list">
+        {data.map((movie) => (
+          <MovieCart movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
