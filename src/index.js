29dd103d4e97de4a346a80387de54880fc31479d
dhwanisarokar <dhwanisarokar@gmail.com>
2022-11-15 17:17:28 +0530

    add middleWare between action and reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 
 import "./index.css";
 import rootReducer from "./reducers/index";
 import App from "./components/App";
 
-const store = createStore(rootReducer);
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       // middleware code
+//       // console.log("ACTION_TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) =>
+    next(action);
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
